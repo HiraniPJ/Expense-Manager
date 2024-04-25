af7e02da2fd81ecb53e9c740aef8c8f3d89d5d8e
HiraniPJ <pooja.hirani93@gmail.com>
2024-04-25 14:01:25 +0100

    Clears terminal for a clean display.

--- run.py
+++ run.py
@@ -1,25 +1,24 @@
+import os
 import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/drive
     ]
 
+def autheticaticate_google_docs():
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-
 SHEET = GSPREAD_CLIENT.open('Expense-Manager')
-
 expenses = SHEET.worksheet('expenses')
-
-data = expenses.get_all_values()
-
-print (data)
-
-
+ 
+def clear_terminal():
+    """ Clears termnals for a clean display. """
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
-
+if __name__=="__main__":
+    main()
