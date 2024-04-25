51d447d062d1055a3201abf3c53fe9fc3882e82e
HiraniPJ <pooja.hirani93@gmail.com>
2024-04-25 14:07:52 +0100

    Prints introductory ASCII art for the application.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 import gspread
 from google.oauth2.service_account import Credentials
+from art import text2art
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -18,6 +19,17 @@ expenses = SHEET.worksheet('expenses')
 def clear_terminal():
     """ Clears termnals for a clean display. """
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_intro():
+    """ Prints Introductory ASCII art for the application. """
+    clear_terminal()
+    intro_art = text2art("Expense Manager") 
+    print(intro_art)
+
+
+
+def main ():
+    print_intro()
 
 
 if __name__=="__main__":
