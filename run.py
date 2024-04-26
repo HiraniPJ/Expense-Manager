e6083e50a30e30c15e384542cb17b1b9278214ac
HiraniPJ <pooja.hirani93@gmail.com>
2024-04-26 05:32:21 +0100

    user interaction for months and  budget selection

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ from art import text2art
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive
+    "https://www.googleapis.com/auth/drive"
     ]
 
-def autheticaticate_google_docs():
+
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
@@ -36,7 +36,26 @@ def validate_input(input_value, data_type):
         return True, value
     except ValueError:
         return False, None    
+    
+def get_month_selection():
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+   
+    print("Select a month:")
+    for i, month in enumerate(months, start=1):
+        print(f"{i}. {month}")
+    while True:
+        month_index = input("nter the number of the month: ")   
+        if month_index.isdigit() and 1 <+ int(month_index) <= 12:
+            return months[int(month_index) -1]
+        print("Invalid input. Please enter a valid number between 1 and 12.")
 
+def set_monthly_budget():
+    month = get_month_selection()
+    while True:
+        budget = input(f"nter the budget for {month}:")
+        valid, budget_value = validate_input(budget, 'float')     
+        if valid: return month, budget_value    
+    
 
 def main ():
     print_intro()
